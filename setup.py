210eb0d6004a3bac8f92d182ddd47214a5176ade
Siva Surendira <136654928+sivasurend@users.noreply.github.com>
2024-01-28 09:14:34 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lyzr-experimental-automata',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'asyncio', # If you're using any non-standard library packages
+    ],
+    author='Siva Surendira',
+    author_email='siva@lyzr.ai',
+    description='A prompt based agent workflow that integrates with other Lyzr agents',
+    long_description=open('README.md').read(),
+    long_description_content_type='check Readme file',
+    url='https://github.com/lyzrcore/lyzr-experimental-automata',
+    license='MIT',
+)
